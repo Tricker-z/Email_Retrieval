6508ba22e2e7de3465efd793f53aa3f5fbdb9db6
Tricker-z <51984425+Tricker-z@users.noreply.github.com>
2020-06-13 20:00:11 +0800

    init

---
+++ email_IR/er_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\大三下\CS332 信息检索\project\EmailRetrieval\Email_Retrieval\email_IR\er_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
